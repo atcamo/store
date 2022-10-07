5ff6ecfba7992de925d60dcb6376e3f58613ad78
atcamo <atiliocm@gmail.com>
2022-10-06 22:47:21 -0300

    store

---
+++ App.js
@@ -0,0 +1,28 @@
+import { validarProductoRepetido } from "./src/accionesCarrito.js";
+
+const mostrarProductos = (productos) => {
+  const contenedorProductos = document.getElementById("producto-contenedor");
+
+  productos.forEach(producto => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML += `<div class="card-image">
+                        <img src=${producto.img}>
+                        <span class="card-title">${producto.nombre}</span>
+                        <a class="btn-floating halfway-fab wabes-effect waves-light green" id=boton${producto.id}><i class="material-icons">add_shopping_cart</i></a>
+                      </div>
+                      <div class="card-content">
+                          <p>Tipo: ${producto.tipo}</p>
+                          <p>${producto.precio} BTC</p>
+                      </div>
+                     `
+    contenedorProductos.appendChild(div);
+
+    const boton = document.getElementById(`boton${producto.id}`);
+    boton.addEventListener('click', () => {
+      validarProductoRepetido(producto.id);
+    })
+  });
+};
+
+export { mostrarProductos };
---
+++ src/stock.js
@@ -0,0 +1,58 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "Knox",
+        tipo: "Fragata",
+        precio: 10,
+        img: 'public/images/Knox.png',
+        cantidad:1
+    },
+
+    {
+        id: 2,
+        nombre: "Oliver",
+        tipo: "Fragata",
+        precio: 50,
+        img: 'public/images/Oliver.png',
+        cantidad:1
+    },
+
+    {
+        id: 3,
+        nombre: "Duque",
+        tipo: "Fragata",
+        precio: 100,
+        img: 'public/images/Duque.png',
+        cantidad:1
+    },
+
+    {
+        id: 4,
+        nombre: "Independence",
+        tipo: "Fragata",
+        precio: 160,
+        img: 'public/images/Independence.png',
+        cantidad:1
+    },
+/*
+    {
+        id: 5,
+        nombre: "Buzo 2",
+        tipo: "buzo",
+        precio: 1200,
+        img: 'public/images/buzo.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 6,
+        nombre: "Remera 2",
+        tipo: "remera",
+        precio: 500,
+        img: 'public/images/remera.jpg',
+        cantidad:1
+    }*/
+];
+
+export { productos };
